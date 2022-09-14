fae154d4b2e8eed552b26d817d78690fa0ee9d34
xgx2003@gmail.com <xgx2003@gmail.com>
2022-09-14 15:18:39 +0900

    test3

--- ref_ex3.cpp
+++ ref_ex3.cpp
@@ -34,7 +34,44 @@ void InsertMember(){
     idx++;//인덱스 번호 하나 증가
     printf("회원정보 등록 완료\n");
 }
-
+void PrintAllMember(){
+    //전체 회원 정보를 출력
+    printf("전체 회원정보 출력\n\n");
+    for(int i=0;i<idx;i++)  
+        printf("%s %d %c\n",arr[i].name, arr[i].age, arr[i].gender);  
+    printf("\n");
+}
+void SearchMember(){
+//이름하나 입력 받아서
+//구조체가 가지고 있는 name과 동일한 데이터를 찾음
+    char str[30];
+    printf("검색할 이름 입력 : ");
+    scanf("%s",str);
+    for(int i=0;i<idx;i++){
+        if(strcmp(arr[i].name,str) == 0){
+            printf("%s %d %c\n",arr[i].name, arr[i].age, arr[i].gender);  
+            return;
+        }
+    }
+    printf("검색 결과가 없습니다.\n");
+}
+void DeleteMember(){
+    //삭제할 이름 입력을 받음
+    char str[30];
+    printf("삭제할 이름 입력 : ");
+    scanf("%s",str);
+    for(int i=0;i<idx;i++){
+        if(strcmp(arr[i].name,str) == 0){
+            //삭제를 수행
+            //배열의 내용을 한칸씩 땡김
+            for(int j=i;j<idx-1;j++)
+                arr[j] = arr[j+1];
+            idx--;//인덱스 값 감소
+            return;
+        }
+    }
+    printf("삭제할 데이터가 없습니다.\n");
+}
 int main(void){
     int no = -1;
 
